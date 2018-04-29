49de4f849288472a02137b9ce0af61ead74e40a1
non.t <nonbody.en.th@hotmail.com>
2018-04-29 21:53:02 +0700

    add world controller and change hello controller

--- src/main/java/com/git/controller/WorldController.java
+++ src/main/java/com/git/controller/WorldController.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class WorldController {
 
-    @GetMapping("/hello")
+    @GetMapping("/world")
     public String hello(){
         return "hello customize";
     }
